7c53c59eaf7e72da4463e0761b55646efa4c4a31
難波隼人 <nanbahayato@nanbahayatonoMacBook-Air.local>
2024-10-08 03:44:40 +0900

    edit-motalwindow

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,33 @@
+import { useState } from 'react';
 import { Button } from '../ui/button'
+import Window from './window';
+
 
 const Header = () => {
+    const [openWindow, setopenWindow] = useState (false);
+
+    const openClick = () => {
+        setopenWindow (true);
+    };
+
+    const closeClick = () => {
+        setopenWindow (false);
+    };
+
   return (
-    <div 
+    <div
         className="fixed flex justify-between px-8 w-screen h-16 bg-gray-500 
         items-center drop-shadow-2xl shadow-md z-10">
             <h1 className="font-bold text-2xl">Meditation-Program</h1>
             <div className="flex gap-3">
-                <Button variant="outline">
-                    <a href="https://ui.shadcn.com/docs">Meditation-Programとは？</a>
-                </Button>
+                <div>
+                    <Button 
+                    onClick={openClick}
+                    variant="outline">
+                        そもそも瞑想とは？
+                    </Button>
+                    <Window openWindow={openWindow} onClose= {closeClick} />
+                </div>
                 <Button>menu</Button>
             </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ export default {
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
+		gridAutoColumns: {
+			'10rem': '10rem',
+		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
