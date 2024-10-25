1706ea08c8674a99427c55f2bb9e95ec90e70f2f
難波隼人 <hayatonanba@gmail.com>
2024-10-25 13:54:08 +0900

    フォーム作成

--- src/components/Footer/form.tsx
+++ src/components/Footer/form.tsx
@@ -24,45 +24,46 @@ const FormQ = () => {
     };
 
   return (
-    <div className="bg-[#f8f4e6] h-screen">
+    <div className="bg-[#f8f4e6] h-screen flex items-center">
         <div className="bg-white shadow-lg rounded-sm w-[40%] mx-auto h-[80%] p-5">
-        <Form {...form}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <h1 className="text-center text-4xl">お問い合わせフォーム</h1>
-                
-                    <FormItem className="mb-4">
-                    <FormLabel htmlFor="name">お名前</FormLabel>
-                    <FormControl>
-                        <Input placeholder="your name" id="name" type="text" {...register("name")} />
-                    </FormControl>
-                    <FormMessage>{errors.name?.message as ReactNode}</FormMessage>
-                    </FormItem>
-        
-                    <FormItem className="mb-4">
-                    <FormLabel htmlFor="email">メールアドレス</FormLabel>
-                    <FormControl>
-                        <Input placeholder="your email" id="email" type="email" {...register("email")} />
-                    </FormControl>
-                    <FormMessage>{errors.email?.message as ReactNode}</FormMessage>
-                    </FormItem>
-        
-                    <FormItem className="mb-4">
-                    <FormLabel htmlFor="content">お問い合わせ内容</FormLabel>
-                    <FormControl>
-                        <Input placeholder="Please your request" id="content" type="text" {...register("content")} />
-                    </FormControl>
-                    <FormMessage>{errors.content?.message as ReactNode}</FormMessage>
-                    </FormItem>
+            <Form {...form}>
+                <form className="flex flex-col gap-y-4" onSubmit={handleSubmit(onSubmit)}>
+                    <h1 className="text-center text-4xl">お問い合わせフォーム</h1>
+                    
+                        <FormItem className="mb-4">
+                        <FormLabel htmlFor="userName">お名前</FormLabel>
+                        <FormControl>
+                            <Input placeholder="your name" id="name" type="text" {...register("name")} />
+                        </FormControl>
+                        <FormMessage>{errors.name?.message as ReactNode}</FormMessage>
+                        </FormItem>
             
-                <Button className="w-40" type="submit">送信する →</Button>
+                        <FormItem className="mb-4">
+                        <FormLabel htmlFor="email">メールアドレス</FormLabel>
+                        <FormControl>
+                            <Input placeholder="your email" id="email" type="email" {...register("email")} />
+                        </FormControl>
+                        <FormMessage>{errors.email?.message as ReactNode}</FormMessage>
+                        </FormItem>
+            
+                        <FormItem className="mb-4">
+                        <FormLabel htmlFor="content">お問い合わせ内容</FormLabel>
+                        <FormControl>
+                            <Input placeholder="Please your request" id="content" type="text" {...register("content")} />
+                        </FormControl>
+                        <FormMessage>{errors.content?.message as ReactNode}</FormMessage>
+                        </FormItem>
+                
+                    <Button className="w-40" type="submit">送信する →</Button>
 
-                <p className="text-center p-5">
-                    皆様からいただいた内容はすべて目を通しておりますが、<br />
-                    ご意見、ご要望に対して回答をお約束するものではありません。<br />                   
-                    ご了承ください。
-                </p>
-            </form>
-        </Form></div>
+                    <p className="text-center p-5">
+                        皆様からいただいた内容はすべて目を通しておりますが、<br />
+                        ご意見、ご要望に対して回答をお約束するものではありません。<br />                   
+                        ご了承ください。
+                    </p>
+                </form>
+            </Form>
+        </div>
     </div>
   );
 };
