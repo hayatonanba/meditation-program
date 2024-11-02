ea60fb46d11d2677cfa53f0c3521875161f04178
難波隼人 <hayatonanba@gmail.com>
2024-11-02 16:17:03 +0900

    fix:db

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const Pool  = require('pg').Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    host: "126.88.239.54",
-    database: "nanbawebsite",
-    password: "Optimus11",
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 module.exports = pool;
